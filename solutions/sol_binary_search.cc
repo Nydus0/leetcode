ae701792a6a3c1a4d6813b9365b05679a64cafcd
Nydus0 <99085768+Nydus0@users.noreply.github.com>
2023-11-04 08:21:17 +0100

    refactor

--- solutions/sol_binary_search.cc
+++ solutions/sol_binary_search.cc
@@ -146,22 +146,3 @@ int sol_binary_search::search_in_rotated_array(vector<int>& nums, int target) {
 
     return result;
 }
-
-TimeMap::TimeMap() {}
-
-void TimeMap::set(string key, string value, int timestamp) {
-    _hashmap[key].push_back(make_pair(value, timestamp));
-}
-
-string TimeMap::get(string key, int timestamp) {
-    // all the timestamps are strictly increasing so we can directly use binary search on timestamps
-    // user upper_bound : we want 1st elt verifying elt.timestamp > timestamp
-    auto it = upper_bound(_hashmap[key].begin(), _hashmap[key].end(), pair<string, int>("", timestamp),
-                          // create lambda to specialize comparison with timestamp => comp(value, element)
-                          [](const pair<string, int>& pair_a, const pair<string, int>& pair_b) {
-                              return pair_a.second < pair_b.second;
-                          });
-
-    // return value of elt just before result of binary search
-    return (it == _hashmap[key].begin()) ? "" : prev(it)->first;
-}
--- solutions/sol_binary_search.hpp
+++ solutions/sol_binary_search.hpp
@@ -2,9 +2,6 @@
 
 #include "solution.hpp"
 
-#include <string>
-#include <unordered_map>
-
 class sol_binary_search : public solution {
    public:
     int search(std::vector<int>& nums, int target);
@@ -12,22 +9,4 @@ class sol_binary_search : public solution {
     int minEatingSpeed(std::vector<int>& piles, int h);
     int findMin(std::vector<int>& nums);
     int search_in_rotated_array(std::vector<int>& nums, int target);
-};
-
-/**
- * Your TimeMap object will be instantiated and called as such:
- * TimeMap* obj = new TimeMap();
- * obj->set(key,value,timestamp);
- * string param_2 = obj->get(key,timestamp);
- */
-
-class TimeMap {
-   public:
-    TimeMap();
-
-    void set(std::string key, std::string value, int timestamp);
-    std::string get(std::string key, int timestamp);
-
-   private:
-    std::unordered_map<std::string, std::vector<std::pair<std::string, int>>> _hashmap;
 };
